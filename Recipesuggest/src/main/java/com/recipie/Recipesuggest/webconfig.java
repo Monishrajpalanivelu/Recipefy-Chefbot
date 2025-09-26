ba28447da20ae0670383782f4f45c7d4c525ce81
Monishrajpalanivelu <142282793+Monishrajpalanivelu@users.noreply.github.com>
2025-09-27 00:16:55 +0530

    Update webconfig.java
    updated live host render

--- Recipesuggest/src/main/java/com/recipie/Recipesuggest/webconfig.java
+++ Recipesuggest/src/main/java/com/recipie/Recipesuggest/webconfig.java
@@ -14,7 +14,7 @@ public class webconfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000/")
+                        .allowedOrigins("https://recipefy-chefbot-backend.onrender.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
